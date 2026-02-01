97a86596f37d7d118bf28b2a070b50753ef7edaf
SaqeefShabbir <saqeef2016@gmail.com>
2026-02-01 20:42:46 +0500

    added a new springboot project

---
+++ digital_twin_springboot/src/main/java/com/digital_twin_airport/digital_twin_springboot/DigitalTwinSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.digital_twin_airport.digital_twin_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalTwinSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalTwinSpringbootApplication.class, args);
+	}
+
+}
---
+++ digital_twin_springboot/src/test/java/com/digital_twin_airport/digital_twin_springboot/DigitalTwinSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digital_twin_airport.digital_twin_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalTwinSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
