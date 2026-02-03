b42c2d0054ce6bfd2b8bfe48d32ecbd7a0a73257
SaqeefShabbir <saqeef2016@gmail.com>
2026-02-03 21:31:49 +0500

    Replaced angular project with modules

--- digital_twin_springboot/src/main/java/com/digital_twin_airport/digital_twin_springboot/DigitalTwinSpringbootApplication.java
+++ digital_twin_springboot/src/main/java/com/digital_twin_airport/digital_twin_springboot/DigitalTwinSpringbootApplication.java
@@ -2,8 +2,20 @@ package com.digital_twin_airport.digital_twin_springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.airport.digitaltwin.model")
+@EnableJpaRepositories(basePackages = "com.airport.digitaltwin.repository")
+@EnableCaching
+@EnableScheduling
+@EnableAsync
+@EnableKafka
 public class DigitalTwinSpringbootApplication {
 
 	public static void main(String[] args) {
